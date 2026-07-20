a4fe6fdcc069b269a0bf323d310b0f7a19a58687
zorr me <zorrme@gmail.com>
2026-07-19 18:05:57 -0700

    chore: configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://zorrme.github.io',
+  base: '/flower-bloom',
+  output: 'static',
+});
